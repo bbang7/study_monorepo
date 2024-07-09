d23a597a37cc8c00ae9dbb31fa89d11973bab889
bbang7 <zxas1245@naver.com>
2024-07-09 17:28:42 +0900

    redo graph check

--- packages/shared-ui/Button.tsx
+++ packages/shared-ui/Button.tsx
@@ -1,5 +1,6 @@
 export function Button(props: any) {
   console.log("my feature");
+  console.log("hello my feature!");
 
   return <button onClick={() => props.onClick()}>{props.children}</button>;
 }
